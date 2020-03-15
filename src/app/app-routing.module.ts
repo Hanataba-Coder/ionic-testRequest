5c9ff2e4d3dd31aecd105e25b0574c002b42c8aa
Yodsapat Pansawat <yodsapat@codediva.co.th>
2020-03-15 16:59:24 +0700

    change redirect page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'detail', pathMatch: 'full' },
+  { path: '', redirectTo: 'photo', pathMatch: 'full' },
   { path: 'photo', loadChildren: () => import('./photo/photo.module').then( m => m.PhotoPageModule) },
   { path: 'detail', loadChildren: () => import('./photo-detail/photo-detail.module').then( m => m.PhotoDetailPageModule)}
 ];
